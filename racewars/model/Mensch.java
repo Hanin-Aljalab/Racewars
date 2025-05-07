a0a3cbe840612f5b0a9a8b18906ce0fd104987c4
Hanin Aljalab <80221822+Hanin-Aljalab@users.noreply.github.com>
2025-05-07 16:40:47 +0200

    Update Mensch.java

--- racewars/model/Mensch.java
+++ racewars/model/Mensch.java
@@ -1,12 +1,12 @@
 package pr2.vererbung.racewars.racewars.model;
 
 /**
- * Klasse zur Repräsentation der Mensch-Rasse im Spiel.
+ * Class representing the Human race in the game.
  */
 public class Mensch extends Rasse {
     /**
-     * Konstruktor für die Erstellung eines Mensch-Objekts.
-     * Initialisiert die Attribute der Mensch-Rasse.
+     * Constructor for creating a Human object.
+     * Initializes the attributes specific to the Human race.
      */
     public Mensch() {
         super("Mensch", 140, 40, 2, 0.4, 110);
