5fe2cd9c5d65e72a64a1eee7b53b06ee8f3ff86b
Hanin Aljalab <80221822+Hanin-Aljalab@users.noreply.github.com>
2025-05-07 16:41:24 +0200

    Update Ork.java

--- racewars/model/Ork.java
+++ racewars/model/Ork.java
@@ -1,12 +1,12 @@
 package pr2.vererbung.racewars.racewars.model;
 
 /**
- * Klasse zur Repräsentation der Ork-Rasse im Spiel.
+ * Class representing the Orc race in the game.
  */
 public class Ork extends Rasse {
     /**
-     * Konstruktor für die Erstellung eines Ork-Objekts.
-     * Initialisiert die Attribute der Ork-Rasse.
+     * Constructor for creating an Orc object.
+     * Initializes the attributes specific to the Orc race.
      */
     public Ork() {
         super("Ork", 100, 33, 1, 0.3, 150);
