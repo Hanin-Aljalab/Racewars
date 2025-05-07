f475ef309ae537b5475131982b7c85cd1aeed3ff
Hanin Aljalab <80221822+Hanin-Aljalab@users.noreply.github.com>
2025-05-07 17:49:42 +0200

    Update Untote.java

--- racewars/model/Untote.java
+++ racewars/model/Untote.java
@@ -1,12 +1,12 @@
 package pr2.vererbung.racewars.racewars.model;
 
 /**
- * Klasse zur Repräsentation der Untote-Rasse im Spiel.
+ * Class representing the Undead race in the game.
  */
 public class Untote extends Rasse {
     /**
-     * Konstruktor für die Erstellung eines Untote-Objekts.
-     * Initialisiert die Attribute der Untote-Rasse.
+     * Constructor for creating an Undead object.
+     * Initializes the attributes specific to the Undead race.
      */
     public Untote() {
         super("Untote", 120, 16, 2, 0.3, 70);
